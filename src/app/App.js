b5f32b962b8517dab9fa041e1af396161995ce14
jackL <s6263269@gmail.com>
2022-09-02 03:32:45 +0800

    "change home page layout"

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,14 @@
-import React, { useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
   useLocation,
   withRouter,
-} from "react-router-dom";
-import AppRoutes from "./routes";
-import Headermain from "../header";
-import AnimatedCursor from "react-animated-cursor";
-import "./App.css";
+} from 'react-router-dom';
+import AppRoutes from './routes';
+import Headermain from '../header';
+import AnimatedCursor from 'react-animated-cursor';
+import './App.css';
 
 function _ScrollToTop(props) {
   const { pathname } = useLocation();
@@ -22,11 +22,11 @@ const ScrollToTop = withRouter(_ScrollToTop);
 export default function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <div className="cursor__dot">
+      <div className='cursor__dot'>
         <AnimatedCursor
           innerSize={15}
           outerSize={15}
-          color="255, 255 ,255"
+          color='255, 255 ,255'
           outerAlpha={0.4}
           innerScale={0.7}
           outerScale={5}
