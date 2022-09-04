2f9a04d06c467a0bc7bc91633cd2c02defda1a63
jackL <s6263269@gmail.com>
2022-09-04 15:17:15 +0800

    add og property

--- src/app/App.js
+++ src/app/App.js
@@ -21,7 +21,7 @@ const ScrollToTop = withRouter(_ScrollToTop);
 
 export default function App() {
   return (
-    <Router basename='/personal_website_v2'>
+    <Router basename='/www.jackliu.site'>
       <div className='cursor__dot'>
         <AnimatedCursor
           innerSize={15}
